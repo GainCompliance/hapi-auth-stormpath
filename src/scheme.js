2a2afa7ea393195a6fde2219018a548d386c2c45
Matt Travi <matt@gaincompliance.com>
2016-12-07 11:32:17 -0600

    fix(validation): removed joi validation because of rollup confusion
    because joi exports a default() method on the validator instance, the consumption resolution logic
    
    thinks that the common-js bundle for this package is an es module, treating that default method as
    
    the default module export. hoping to find a way to resolve this through
    
    https://github.com/rollup/rollup/issues/1135, but removing the joi dependency to get this working
    
    for node consumers in the meantime
    
    revert this commit as part of #2

--- src/scheme.js
+++ src/scheme.js
@@ -1,39 +1,22 @@
-import joi from 'joi';
-import hoek from 'hoek';
 import nJwt from 'njwt';
 import Boom from 'boom';
 
-function validate(options) {
-  const validated = joi.validate(options, joi.object({
-    applicationHref: joi.string().uri().required(),
-    apiKeyId: joi.string().required(),
-    apiKeySecret: joi.string().required(),
-    returnUrl: joi.string().uri().required()
-  }).required());
-
-  hoek.assert(!validated.error, validated.error);
-
-  return validated.value;
-}
-
 export function scheme(server, options) {
-  const settings = validate(options);
-
   return {
     authenticate(request, reply) {
       const jwtResponse = request.query.jwtResponse;
 
       if (!jwtResponse) {
         const jwt = nJwt.create({
-          iss: settings.apiKeyId,
-          sub: settings.applicationHref,
-          cb_uri: settings.returnUrl,
+          iss: options.apiKeyId,
+          sub: options.applicationHref,
+          cb_uri: options.returnUrl,
           state: JSON.stringify(request.url.query)
-        }, settings.apiKeySecret);
+        }, options.apiKeySecret);
 
         reply.redirect(`https://api.stormpath.com/sso?jwtRequest=${jwt.compact()}`);
       } else {
-        nJwt.verify(jwtResponse, settings.apiKeySecret, (err, verifiedJwt) => {
+        nJwt.verify(jwtResponse, options.apiKeySecret, (err, verifiedJwt) => {
           if (err) {
             reply(Boom.wrap(err));
           } else if ('AUTHENTICATED' !== verifiedJwt.body.status) {
